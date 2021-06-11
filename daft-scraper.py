0b26fc1de4d0ce72590ec77edb0080b11a572c9a
sWrAAb <swraab@gmail.com>
2021-06-11 11:33:45 +0100

    added user inputs

--- daft-scraper.py
+++ daft-scraper.py
@@ -4,3 +4,29 @@ from daft_scraper.search.options import (
     PriceOption, BedOption
 )
 from daft_scraper.search.options_location import LocationsOption, Location
+
+# credits - https://pypi.org/project/daft-scraper/
+
+def user_inputs():
+
+    property_type = input('House or Apartment (h/a)')
+    if property_type == 'h' or property_type == 'H':
+        property_type = PropertyType.HOUSE
+    elif property_type == 'a' or property_type == 'A':
+        property_type = PropertyType.HOUSE
+    else:
+        print('Invalid input. Please enter h for house or a for apartment.')
+        property_type = input('House or Apartment (h/a)')
+    
+    rent_range = int(input('Maximum rent: '))
+    
+    bedrooms = int(input('Bedrooms: '))
+
+
+
+    api = DaftSearch(SearchType.RENT)
+    listings = api.search(options)
+
+    return listings
+
+user_inputs()
