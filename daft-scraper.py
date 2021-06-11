d4238d249238d54af7926c67c901fc4c2b06893c
sWrAAb <swraab@gmail.com>
2021-06-11 10:42:35 +0100

    Initial commit.

---
+++ daft-scraper.py
@@ -0,0 +1,6 @@
+from daft_scraper.search import DaftSearch, SearchType
+from daft_scraper.search.options import (
+    PropertyType, PropertyTypesOption, Facility, FacilitiesOption,
+    PriceOption, BedOption
+)
+from daft_scraper.search.options_location import LocationsOption, Location
