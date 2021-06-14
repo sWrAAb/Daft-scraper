3ad6cf069452a12e7910a01d54095deac0a22f6c
sWrAAb <swraab@gmail.com>
2021-06-14 20:30:46 +0100

    added requirements.txt for daft library, added comments and messsage ifno  matches found

--- daft-scraper.py
+++ daft-scraper.py
@@ -1,3 +1,4 @@
+# Import library - https://pypi.org/project/daft-scraper/
 from daft_scraper.search import DaftSearch, SearchType
 from daft_scraper.search.options import (
     PropertyType, PropertyTypesOption, Facility, FacilitiesOption,
@@ -6,11 +7,16 @@ from daft_scraper.search.options import (
 from daft_scraper.search.options_location import LocationsOption, Location
 import csv
 
+# Global variables
 
-# credits - https://pypi.org/project/daft-scraper/
+listings = None
+rows = []
 
+# User inputs
 def inputs():
+    global listings
 
+    # Property type
     property_type = input('House or Apartment (h/a)')
     if property_type.lower() == 'h':
         property_type = PropertyType.HOUSE
@@ -19,13 +25,17 @@ def inputs():
     else:
         print('Invalid input. Please enter h for house or a for apartment.')
         property_type = input('House or Apartment (h/a)')
-    
+
+    # Maximum rent
     rent_range = int(input('Maximum rent: '))
     
+    # Number of bedrooms
     bedrooms = int(input('Number of bedrooms: '))
 
+    # CCT college area
     area = Location.DUBLIN_2_DUBLIN
 
+    # Search parameters
     options = [    
             PropertyTypesOption([property_type]),   
             PriceOption(0, rent_range),
@@ -39,13 +49,11 @@ def inputs():
 
     return listings
 
+# Sorting and printing results into terminal and CSV file
+
 def results(listings):
 
-    rows = []
-
-    # Credits
-    # https://stackoverflow.com/questions/3766633/how-to-sort-with-lambda-in-python
-    # https://stackoverflow.com/questions/64016495/sorting-an-array-with-respect-to-values-of-an-other-array-one-after-the-other
+    # Credits - https://stackoverflow.com/questions/3766633/how-to-sort-with-lambda-in-python
 
     sorted_list = sorted(list(listings), key=lambda a: a.price, reverse=False)
 
@@ -55,18 +63,24 @@ def results(listings):
         # Print results in csv file
         rows.append([getattr(listing, 'id'), getattr(listing, 'price'), getattr(listing, 'title'), getattr(listing, 'url')])
 
+    if not rows:
+        print("\n No results match your search criteria")
+
     return rows
 
 def print_results_to_csv(rows):
 
-    #credits https://www.geeksforgeeks.org/python-save-list-to-csv/
+    # credits https://www.geeksforgeeks.org/python-save-list-to-csv/
 
-    with open('daft_search_results.csv', 'w') as f:    
+    with open('daft_search_results.csv', 'w') as f: 
+        # Columns   
         fields = ['Id','Price','Address', 'Url']
         # using csv.writer method from CSV package
         write = csv.writer(f)    
+        # Write columns
         write.writerow(fields)
+        # Write rows
         write.writerows(rows)
 
-
 print_results_to_csv(results(inputs()))
+
